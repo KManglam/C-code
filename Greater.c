6e2894735365ea88f5642cc30006e33f0290877a
Kumar Manglam <kmglm04718@gmail.com>
2025-01-27 22:05:58 +0530

    Update Greater.c

--- Greater.c
+++ Greater.c
@@ -6,8 +6,9 @@ int main(){
 	printf("Enter Second number: ",b);
 	scanf("%d",&b);
 	if (a>b){
-		printf("%d is greater than %d /n",a,b);
-	}else printf("%d is smaller ",b);
+		printf("%d is greater than %d /n",a,b);}
+	else 
+		printf("%d is smaller ",b);
 	
 	return 0;
 }
