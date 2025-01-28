d8a62b07e4d8332c7c02f00b4b61a5ccf4d12c00
Kumar Manglam <kmglm04718@gmail.com>
2025-01-28 12:53:41 +0530

    Update ponter.c

--- ponter.c
+++ ponter.c
@@ -5,11 +5,11 @@ int main() {
 int i=3;
 int *j;
 j=&i;
-printf("Address of i = %p\n",&i);
+printf("Address of i = %p\n",&i);   
 printf("Address of i = %p\n",j);
 printf("Address of j = %p\n",&j);
 printf("Value of j = %p\n",j);
-printf("Value of i = %d\n",i);
+printf("Value of i = %d\n",i);         // 3
 printf("Value of i = %d\n",*(&i));
 printf("Value of i = %d\n",*j);
 return 0;
