12c01e0634014ca2233e021dd01ef20dfcbe0572
Kumar Manglam <kmglm04718@gmail.com>
2025-01-28 09:08:23 +0530

    Update AreaOfRectangle.c

--- AreaOfRectangle.c
+++ AreaOfRectangle.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
 int main(){
+	
 	float length,breadth,Area, Perimeter;
 	Perimeter=2*(length+breadth);
 	Area=length*breadth;
@@ -13,4 +14,5 @@ int main(){
 	printf("Area of Rectangle: %.2f\n",Area);
 	printf("Perimeter of Rectangle: %f\n",Perimeter);
 	return 0;
+	
 }
